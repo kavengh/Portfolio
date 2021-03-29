9ce829f2c80b188e1b630c6719b071f2b20b18df
Kaven Vohra <vohrakaven@gmail.com>
2021-03-29 16:29:43 -0400

    updated components

--- personal-site/src/App.js
+++ personal-site/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import MenuAppBar from "./Components/AppBar/MenuAppBar";
 
-function App() {
+import SimpleContainer from "./Components/AppBar/SimpleContainer";
+
+const App = (props) => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MenuAppBar></MenuAppBar>
+      <SimpleContainer></SimpleContainer>
+      <SimpleContainer></SimpleContainer>
+      <div id="hehe"></div>
     </div>
   );
-}
+};
 
 export default App;
