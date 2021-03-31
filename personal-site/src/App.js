2bcdd04a3f88a3ad1710fda34947bd74d869958f
Kaven Vohra <vohrakaven@gmail.com>
2021-03-31 10:53:24 -0400

    added project display component

--- personal-site/src/App.js
+++ personal-site/src/App.js
@@ -1,17 +1,32 @@
 import React, { useState } from "react";
 import "./App.css";
 import MenuAppBar from "./Components/AppBar/MenuAppBar";
-
 import SimpleContainer from "./Components/AppBar/SimpleContainer";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "./theme";
+import ProjectDisplay from "./ProjectDisplay";
 
 const App = (props) => {
   return (
-    <div className="App">
-      <MenuAppBar></MenuAppBar>
-      <SimpleContainer></SimpleContainer>
-      <SimpleContainer></SimpleContainer>
-      <div id="hehe"></div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <MenuAppBar></MenuAppBar>
+        <div style={{ padding: "40px", backgroundColor: "#f4d58d" }}></div>
+
+        <SimpleContainer info="Eli">
+          hello bye hello bye hello bye hello bye hello bye hello bye hello bye
+          hello bye hello bye hello bye hello bye hello bye hello bye hello bye
+          hello bye hello bye hello bye hello bye hello bye hello bye hello bye
+          hello bye hello bye hello bye hello bye hello bye hello bye hello bye
+          hello bye hello bye hello bye hello bye hello bye hello bye hello bye
+          hello bye
+        </SimpleContainer>
+        <SimpleContainer>
+          <ProjectDisplay></ProjectDisplay>
+        </SimpleContainer>
+        <div id="hehe"></div>
+      </div>
+    </ThemeProvider>
   );
 };
 
