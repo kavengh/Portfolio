00da081c3b2a51fd50e8457eba03baca70672a98
Kaven Vohra <vohrakaven@gmail.com>
2021-03-31 10:55:41 -0400

    added styling and props

--- personal-site/src/Components/AppBar/SimpleContainer.js
+++ personal-site/src/Components/AppBar/SimpleContainer.js
@@ -1,25 +1,33 @@
 import React from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   Container: {
-    padding: 10,
+    backgroundColor: "#f4d58d",
+    color: "black",
+    padding: "50px",
+  },
+  seperation: {
+    backgroundColor: "#32312F",
+    height: "100px",
+  },
+  innerdiv: {
+    width: "90%",
+    margin: "auto",
+    padding: "2rem",
+    backgroundColor: "#333533",
+    borderRadius: "25px",
+    color: "white",
   },
 }));
 
-export default function SimpleContainer() {
+export default function SimpleContainer(props) {
   const classes = useStyles();
   return (
     <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="lg" className={classes.Container}>
-        <Typography
-          component="div"
-          style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
-        />
+      <Container maxWidth="false" className={classes.Container}>
+        <div className={classes.innerdiv}>{props.children}</div>
       </Container>
     </React.Fragment>
   );
