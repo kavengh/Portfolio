799c58f3a97bab2756c32aea6d93ce9d340f7fed
Kaven Vohra <vohrakaven@gmail.com>
2021-03-31 10:54:48 -0400

    added overall theme

---
+++ personal-site/src/theme.js
@@ -0,0 +1,15 @@
+import { createMuiTheme } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#333533",
+    },
+    secondary: {
+      main: "#f4d58d",
+      contrastText: "#a5a58d",
+    },
+  },
+});
+
+export default theme;
